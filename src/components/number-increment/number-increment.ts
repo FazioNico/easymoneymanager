91b403f938c2c292eb8b6062a50c591d2d0aa0fa
FazioNico <contact@nicolasfazio.ch>
2016-11-06 20:45:52 +0100

    rmv console.log(SimpleChanges)

--- src/components/number-increment/number-increment.ts
+++ src/components/number-increment/number-increment.ts
@@ -23,7 +23,7 @@ export class NumberIncrement implements OnChanges{
   }
 
   ngOnChanges(changes: SimpleChanges){
-    console.log('simple change')
+    //console.log('simple change')
     this.solde = -0; // reset solde if input data === 0
     this.increment();
   }
